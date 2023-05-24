b8d36676fa6828e64a7326b339212a9fb75225a0
Nastya <asenka0301@gmail.com>
2023-05-24 12:08:23 +0300

    make get request to google api books

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
-const SearchForm = () => (
-  <Form className="d-flex justify-content-end">
-    <Form.Control
-      type="search"
-      placeholder="Aвтор или название книги"
-      className="me-2"
-      autoFocus
-    />
-    <Button variant="success" type="submit">Поиск</Button>
-  </Form>
-);
+const SearchForm = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${inputValue}&key=${key}`);
+      console.log(response.data.items);
+    } catch (errors) {
+      console.log(errors);
+    }
+  };
+
+  return (
+    <Form className="d-flex justify-content-end" onSubmit={handleSubmit}>
+      <Form.Control
+        type="search"
+        placeholder="Aвтор или название книги"
+        className="me-2"
+        autoFocus
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <Button variant="success" type="submit">Поиск</Button>
+    </Form>
+  );
+};
 
 export default SearchForm;
