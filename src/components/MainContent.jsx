1f14dd462e8213905917f25306b7d09a11696bc6
Nastya <asenka0301@gmail.com>
2023-07-10 12:23:35 +0300

    add book preload

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import PageControls from './PageControls';
 import Books from './Books';
 import useLoad from '../hooks';
 import Loader from './Loader';
 import Pagination from './Pagination';
+import { addBooks } from '../slice/booksSlice';
 
 const MainContent = (props) => {
   const load = useLoad();
+  const dispatch = useDispatch();
   const {
     currentPage,
     setCurrentPage,
@@ -16,6 +20,26 @@ const MainContent = (props) => {
     sortedBy,
     setSortedBy,
   } = props;
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      load.setLoading(true);
+
+      const response = await axios.get('https://openlibrary.org/search.json?q=javascript');
+      const searchResult = response.data.docs;
+
+      if (searchResult.length > 0) {
+        load.setIsBooksFounded(true);
+        dispatch(addBooks(searchResult));
+        setCurrentPage(1);
+      } else {
+        load.setIsBooksFounded(false);
+      }
+      load.setLoading(false);
+    };
+    fetchContent();
+  }, []);
+
   return (
     <Container className="mh-100 d-flex flex-column">
       <PageControls
